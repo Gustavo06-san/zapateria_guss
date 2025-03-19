9c3bdc66a64a067dbac9db3f5bf23c7d0d251bd3
Gustavo Santos <a23311013.uthermosillo.edu.mx>
2025-03-19 11:11:31 -0700

    nuevos cambios guss

---
+++ database/migrations/2025_03_12_042229_create_detalles_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('detalles', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('id_venta');
+            $table->unsignedBigInteger('id_producto');
+            $table->timestamps();
+
+            // Llaves foráneas
+            $table->foreign('id_venta')->references('id')->on('ventas')->onDelete('cascade');
+            $table->foreign('id_producto')->references('id')->on('productos')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('detalles');
+    }
+};
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ClientesController;
+use App\Http\Controllers\DetallesController;
+use App\Http\Controllers\ProductosController;
+use App\Http\Controllers\VentasController;
+use App\Http\Controllers\HomeController;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
 });
